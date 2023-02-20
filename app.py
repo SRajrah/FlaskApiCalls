c5a4a42e877041fee89b27615f498033333c1dd8
srajrah <shubhamrajrah@gmail.com>
2023-02-20 11:24:50 -0800

    retracted key

--- app.py
+++ app.py
@@ -11,24 +11,24 @@ def hello_world():
 
 @app.route("/")
 def get_movies():
-    url = "https://api.themoviedb.org/3/discover/movie?api_key=94a1cf13847394a4edf36f1f0ca04fbe"
+    url = "https://api.themoviedb.org/3/discover/movie?api_key="
     print("***********************",url)
     response = urllib.request.urlopen(url)
     data = response.read()
     dict = json.loads(data)
+    print("printing **************",dict)
 
     return render_template ("movies.html", movies=dict["results"])
 
 @app.route("/movies")
 def get_movies_list():
-    url = "https://api.themoviedb.org/3/discover/movie?api_key=94a1cf13847394a4edf36f1f0ca04fbe"
+    url = "https://api.themoviedb.org/3/discover/movie?api_key="
 
     response = urllib.request.urlopen(url)
     movies = response.read()
     dict = json.loads(movies)
 
     movies = []
-
     for movie in dict["results"]:
         movie = {
             "title": movie["title"],
